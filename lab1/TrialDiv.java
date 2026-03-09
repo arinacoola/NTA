5d7c74624227c286690201a82c4914be83864019
arinacoola <a.zvarunchyk@gmail.com>
2026-03-10 01:38:51 +0200

    +перевірка

--- lab1/TrialDiv.java
+++ lab1/TrialDiv.java
@@ -3,6 +3,9 @@ import java.math.BigInteger;
 public class TrialDiv {
     private static final int[] PRIMES ={2,3,5,7,11,13,17,19,23,29,31,37,41,43,47};
     public static BigInteger trialDivision(BigInteger n) {
+        if (n.compareTo(BigInteger.ONE) <= 0) {
+            return null;
+        }
         for (int p :PRIMES) {
             BigInteger div = BigInteger.valueOf(p);
             if (n.mod(div).equals(BigInteger.ZERO)) {
