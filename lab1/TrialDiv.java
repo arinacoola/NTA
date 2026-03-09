a08fc9cd65b3a3a447e81ef2a4377e8fee1e787e
arinacoola <a.zvarunchyk@gmail.com>
2026-03-10 01:36:41 +0200

    адекватне

--- lab1/TrialDiv.java
+++ lab1/TrialDiv.java
@@ -1,22 +1,14 @@
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.List;
 
 public class TrialDiv {
     private static final int[] PRIMES ={2,3,5,7,11,13,17,19,23,29,31,37,41,43,47};
-    public static List<BigInteger> trialDivision(BigInteger n) {
-        List<BigInteger> div =new ArrayList<>();
-        BigInteger cur =n;
+    public static BigInteger trialDivision(BigInteger n) {
         for (int p :PRIMES) {
-            BigInteger divisor = BigInteger.valueOf(p);
-            while (cur.mod(divisor).equals(BigInteger.ZERO)) {
-                div.add(divisor);
-                cur = cur.divide(divisor);
+            BigInteger div = BigInteger.valueOf(p);
+            if (n.mod(div).equals(BigInteger.ZERO)) {
+                return div;
             }
         }
-        if (!cur.equals(BigInteger.ONE)) {
-            div.add(cur);
-        }
-        return div;
+        return null;
     }
 }
