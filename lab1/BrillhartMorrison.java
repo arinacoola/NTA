72b8565705e217094b7345887291bed1f3c924d4
arinacoola <a.zvarunchyk@gmail.com>
2026-03-11 01:56:08 +0200

    формування факторної бази

---
+++ lab1/BrillhartMorrison.java
@@ -0,0 +1,53 @@
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BrillhartMorrison {
+    List<Integer> factorBase = new ArrayList<>();
+    public void buildFactorBase(BigInteger n) {
+        factorBase.add(-1);
+        double ln = Math.log(n.doubleValue());
+        double lnln = Math.log(ln);
+        double L = Math.exp(Math.sqrt(ln * lnln));
+        double a = 1.0 / Math.sqrt(2);
+        int upBound = (int) Math.pow(L, a);
+        for (int p = 2; p <= upBound; p++) {
+            if (!isPrime(p)) {
+                continue;
+            }
+            if (p == 2) {
+                continue;
+            }
+            if (legendre(n, p) == 1) {
+                factorBase.add(p);
+            }
+        }
+    }
+    private boolean isPrime(int x) {
+        if (x < 2) {
+            return false;
+        }
+        for (int i= 2; i *i <= x; i++) {
+            if (x % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int legendre(BigInteger n,int p) {
+        BigInteger mod = BigInteger.valueOf(p);
+        BigInteger pow = BigInteger.valueOf((p - 1) / 2);
+        BigInteger val = n.mod(mod).modPow(pow, mod);
+        if (val.equals(BigInteger.ZERO)) {
+            return 0;
+        }
+        if (val.equals(BigInteger.ONE)) {
+            return 1;
+        }
+        if (val.equals(mod.subtract(BigInteger.ONE))) {
+            return -1;
+        }
+        return 0;
+    }
+}
