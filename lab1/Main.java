5c7ee0f0577054356ccdaff920e26a6689084f6a
arinacoola <a.zvarunchyk@gmail.com>
2026-03-17 01:12:16 +0200

    пошук канонічного розкладу  числа варіанту 6

---
+++ lab1/Main.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        BigInteger n = new BigInteger("1449863225586482579");
+        CanonicalFactor canonFactor = new CanonicalFactor();
+        List<BigInteger> factorization = canonFactor.findCanonicalFactorization(n);
+        System.out.println();
+        System.out.println("result as list: " + factorization);
+        canonFactor.printResult();
+    }
+}
