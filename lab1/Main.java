13a10c9317597f949a9e0f8ba60ff5befaac2a96
arinacoola <a.zvarunchyk@gmail.com>
2026-03-24 00:01:25 +0200

    переробила трішки мейн щоб працювати з докером

--- lab1/Main.java
+++ lab1/Main.java
@@ -1,11 +1,28 @@
 import java.math.BigInteger;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        BigInteger n = new BigInteger("1449863225586482579");
+        if (args.length == 0) {
+            System.out.println("usage: java Main <number>");
+            System.out.println("example: java Main 1449863225586482579");
+            return;
+        }
+        BigInteger n;
+        try {
+            n = new BigInteger(args[0]);
+        }
+        catch (NumberFormatException e) {
+            System.out.println("input must be an integer");
+            return;
+        }
+        if (n.compareTo(BigInteger.TWO) < 0) {
+            System.out.println("number must be greater than 1");
+            return;
+        }
+        System.out.println("input number: " + n);
+        System.out.println();
         CanonicalFactor canonFactor = new CanonicalFactor();
-        List<BigInteger> factorization = canonFactor.findCanonicalFactorization(n);
+        canonFactor.findCanonicalFactorization(n);
         System.out.println();
         canonFactor.printResult();
     }
